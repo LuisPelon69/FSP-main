c67fac1ced2afaeffed33af0b99a43966f3d5a42
ls603 <ls6033293@gmail.com>
2024-07-08 12:22:45 -0600

    MVC funcionando

--- MVC_VISTAS/Sidebar.php
+++ MVC_VISTAS/Sidebar.php
@@ -5,7 +5,7 @@ es del archivo Gestionar.php (igual te falto crear ese por eso se iba al index p
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
 <!-- Sidebar - Brand -->
-<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="Admin.php">
     <div class="sidebar-brand-icon rotate-n-15">
         <i class="fas fa-laugh-wink"></i>
     </div>
@@ -17,7 +17,7 @@ es del archivo Gestionar.php (igual te falto crear ese por eso se iba al index p
 
 <!-- Nav Item - Dashboard -->
 <li class="nav-item active">
-    <a class="nav-link" href="index.php">
+    <a class="nav-link" href="Admin.php">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Panel de Gestión</span></a>
 </li>
