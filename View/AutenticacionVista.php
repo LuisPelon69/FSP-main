3cc8d75e75202ed6bc76733e1de3e342e0cbc765
ls603 <ls6033293@gmail.com>
2024-07-09 16:39:45 -0600

    Ultimos cambios

--- View/AutenticacionVista.php
+++ View/AutenticacionVista.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+< lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="../css/Style_login.css">
-    <title>Login</title>
+    <link rel="stylesheet" href="../FSP-main-2/css/Style_login.css">
+    <title>Login</title> 
 </head>
 
 <div class="container" id="container">
@@ -51,8 +51,8 @@
             </div>
         </div>
     </div>
-
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js"></script>
-    <script src="../js/Script_login.js"></script>
+    <script src="js\Script_login.js"></script>
+</div>
