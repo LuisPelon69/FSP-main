a04aa55248c7ad5d8824783ccea8cba378821a89
AlfredoFlores <floresibarraalfredo@gmail.com>
2024-07-10 10:32:04 -0600

    TarjetasPHP CORREGIDO

--- Tarjeta.php
+++ Tarjeta.php
@@ -12,14 +12,14 @@
     <title>FSP Administrador</title>
 
     <!-- Custom fonts for this template-->
-    <link href="../FSP-main-1/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="../FSP-main-2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="../FSP-main-1/css/tarjeta.css">
+        <link rel="stylesheet" type="text/css" href="../FSP-main-2/css/tarjeta.css">
 
     <!-- Custom styles for this template-->
-    <link href="../FSP-main-1/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="../FSP-main-2/css/sb-admin-2.min.css" rel="stylesheet">
 
 
     <!--TABLA DE CLIENTES-->
@@ -39,7 +39,7 @@
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             function fetchClientes() {
-                fetch('../FSP-main-1/controller/cliente_controller.php', {
+                fetch('../FSP-main-2/controller/cliente_controller.php', {
                     method: 'GET'
                 })
                 .then(response => response.json())
@@ -60,11 +60,11 @@
     
                         // Nombre completo
                         let cellNombre = row.insertCell(1);
-                        cellNombre.textContent = `${cliente.NombreClien} ${cliente.ApellidoP} ${cliente.ApellidoM}`;
+                        cellNombre.textContent = ${cliente.NombreClien} ${cliente.ApellidoP} ${cliente.ApellidoM};
     
                         // Saldo
                         let cellSaldo = row.insertCell(2);
-                        cellSaldo.textContent = `$ ${cliente.Saldo}`;
+                        cellSaldo.textContent = $ ${cliente.Saldo};
     
                         // Correo
                         let cellCorreo = row.insertCell(3);
@@ -153,7 +153,7 @@
                 const form = document.getElementById('editForm');
     
                 // Obtener los datos del cliente seleccionado
-                fetch(`../FSP-main-1/controller/cliente_controller.php?id=${id}`, {
+                fetch(../FSP-main-2/controller/cliente_controller.php?id=${id}, {
                     method: 'GET'
                 })
                 .then(response => response.json())
@@ -183,7 +183,7 @@
                     Correo: form.elements['Correo'].value
                 };
     
-                fetch('../FSP-main-1/controller/cliente_controller.php', {
+                fetch('../FSP-main-2/controller/cliente_controller.php', {
                     method: 'PUT',
                     headers: {
                         'Content-Type': 'application/json'
@@ -230,7 +230,7 @@
                 const form = document.getElementById('deleteForm');
                 const ids = form.elements['ids'].value.split(',');
     
-                fetch('../FSP-main-1/controller/cliente_controller.php', {
+                fetch('../FSP-main-2/controller/cliente_controller.php', {
                     method: 'DELETE',
                     headers: {
                         'Content-Type': 'application/json'
@@ -284,100 +284,109 @@
         <!-- Sidebar -->
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../FSP-main-1/Index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">FSP Admin<sup>2</sup></div>
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="Admin.php">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fas fa-laugh-wink"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">FSP Admin<sup>©</sup></div>
+        </a>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active">
+            <a class="nav-link" href="Admin.php">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Panel de Gestión</span></a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Interfaz de Administración
+        </div>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree"
+                aria-expanded="true" aria-controls="collapseThree">
+                <i class="fas fa-fw fa-money-bill"></i>
+                <span>Cobros</span>
             </a>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Panel de Gestión</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Interfaz de Administración
-            </div>
-
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Tarjetas</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Operaciones con Tarjetas:</h6>
-                        <a class="collapse-item" href="buttons.html">Nueva Tarjeta</a>
-                        <a class="collapse-item" href="cards.html">Recargar Tarjeta</a>
-                    </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Operaciones de Cobros:</h6>
+                    <a class="collapse-item" href="Nuevo_Cobro.php">Nuevo Cobro</a>
+                    <a class="collapse-item" href="Historial_Cobros.php">Historial de Cobros</a>
                 </div>
-            </li>
-
-
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Administración</span>
-                </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Menú de Administración:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Nuevo Empleado</a>
-                        <a class="collapse-item" href="utilities-border.html">Agregar Producto</a>
-                        <a class="collapse-item" href="utilities-animation.html">Gestionar elementos</a>
-                        <a class="collapse-item" href="utilities-other.html">Proovedores</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Reportes y Gráficas
             </div>
+        </li>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Cobros</span>
-                </a>
-            </li>    
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Tarjetas</span>
+            </a>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Operaciones con Tarjetas:</h6>
+                    <a class="collapse-item" href="Tarjeta.php">Tarjetas</a>
+                    <a class="collapse-item" href="Recargar_Tarjeta.php">Recargar Tarjeta</a>
+                </div>
+            </div>
+        </li>
 
+        <!-- Nav Item - Utilities Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                aria-expanded="true" aria-controls="collapseUtilities">
+                <i class="fas fa-fw fa-wrench"></i>
+                <span>Administración</span>
+            </a>
+            <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Menú de Administración:</h6>
+                    <a class="collapse-item" href="Empleados.php">Empleados</a>
+                    <a class="collapse-item" href="Agregar_Productos.php">Agregar Producto</a>
+                    <a class="collapse-item" href="Gestionar.php">Gestionar elementos</a> <!--Esta referencia tenía: ?controller=MenuGestionar&action=index-->
+                    <a class="collapse-item" href="Proveedores.php">Proovedores</a>
+                </div>
+            </div>
+        </li>
 
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Gráficas</span></a>
-            </li>
+        <!-- Divider -->
+        <hr class="sidebar-divider">
 
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Reportes y Gráficas
+        </div>
 
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tablas de Datos</span></a>
-            </li>
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                aria-expanded="true" aria-controls="collapsePages">
+                <i class="fas fa-fw fa-folder"></i>
+                <span>Cobros</span>
+            </a>
+        <!-- Nav Item - Charts -->
+        <li class="nav-item">
+            <a class="nav-link" href="charts.html">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Gráficas</span></a>
+        </li>
+
+        <!-- Nav Item - Tables -->
+        <li class="nav-item">
+            <a class="nav-link" href="tables.html">
+                <i class="fas fa-fw fa-table"></i>
+                <span>Tablas de Datos</span></a>
+        </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
@@ -591,9 +600,9 @@
                                     Activity Log
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
+                                <a class="dropdown-item" href="Index.html">
+                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Logout
                                 </a>
                             </div>
                         </li>
@@ -638,7 +647,7 @@
                         </div>
                     </div>
                     
-                    <script href="../FSP-main-1/js/tarjetas_js/tabla_tarjetas.js"></script>
+                    <script href="../FSP-main-2/js/tarjetas_js/tabla_tarjetas.js"></script>
                    <!-- Modal de Altas-->
 <div id="modal" class="modal">
     <div class="modal-content">
@@ -777,15 +786,15 @@
 
 
     <!-- Bootstrap core JavaScript-->
-    <script src="../FSP-main-1/vendor/jquery/jquery.min.js"></script>
-    <script src="../FSP-main-1/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../FSP-main-2/vendor/jquery/jquery.min.js"></script>
+    <script src="../FSP-main-2/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="../FSP-main-1/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="../FSP-main-2/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="../FSP-main-1/js/sb-admin-2.min.js"></script>
-    <script src="../FSP-main-1/js/tarjetas_js/altas_tarjetas.js"></script> <!--Cuando se metan a View o a cualquier otra carpeta a TODAS las rutas se le elimina el "/FSP-main-1" para que tome las rutas correctamente.-->
+    <script src="../FSP-main-2/js/sb-admin-2.min.js"></script>
+    <script src="../FSP-main-2/js/tarjetas_js/altas_tarjetas.js"></script> <!--Cuando se metan a View o a cualquier otra carpeta a TODAS las rutas se le elimina el "/FSP-main-1" para que tome las rutas correctamente.-->
     
     </div>
 </body>
