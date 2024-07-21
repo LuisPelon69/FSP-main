4d781d9c2a19dba36c4ec08a6fa322c8c6e919a0
SantiagoPuc <santyagochelpuc@gmail.com>
2024-07-21 14:48:43 -0600

    .

--- View/Topbar.php
+++ View/Topbar.php
@@ -177,7 +177,7 @@
                 Activity Log
             </a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="Index.html">
+            <a class="dropdown-item" href="Index.php">
                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                 Logout
             </a>
