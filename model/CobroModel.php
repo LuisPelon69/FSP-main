7dbd789f4c747005fea7cd881e4906513a6835c2
julia_la <juliantuyub05@gmal.com>
2024-07-23 15:23:10 -0600

    el model segun pide los precios a la base de datos

--- model/CobroModel.php
+++ model/CobroModel.php
@@ -29,8 +29,29 @@ class CobroModel {
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    
+    // -------------------------------------------------------
+    // Método para obtener los precios de los productos
+    public function getPrecios() {
+        $precios = [];
+
+        // Obtener precios de tamaños de papel
+        $query = "SELECT NombreTam, PreciopUTaP FROM tamañopapel";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $precios['tamañosPapel'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // Obtener precios de tipos de papel
+        $query = "SELECT NombreTipoP, PreciopUTiP FROM tipopapel";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $precios['tiposPapel'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // Obtener precios de tipos de impresión
+        $query = "SELECT NombreTipoI, PreciopUTiI FROM tipoimpresion";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $precios['tiposImpresion'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $precios;
+    }
 }
-
-
-?>
