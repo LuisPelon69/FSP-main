1f6eb9e460a70ccb6a8dc09d001bc76ef96ef55b
julia_la <juliantuyub05@gmal.com>
2024-07-29 23:48:57 -0600

    modificaciones ala grafica

--- View/charts2.php
+++ View/charts2.php
@@ -10,6 +10,18 @@
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <style>
+        .chart-container {
+            position: relative;
+            height: 300px; /* Ajusta la altura según tus necesidades */
+            width: 100%;
+        }
+        .chart-pie-container {
+            position: relative;
+            height: 200px; /* Ajusta la altura según tus necesidades */
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
 
@@ -103,7 +115,7 @@
     <!-- Content Row -->
     <div class="row">
         <!-- Ganancias Mensuales Chart -->
-        <div class="col-xl-8 col-lg-7">
+        <div class="col-xl-8 col-lg-8">
             <div class="card shadow mb-4">
                 <!-- Card Header -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -112,8 +124,8 @@
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
-                    <div class="chart-area">
-                        <canvas id="myAreaChart"></canvas>
+                    <div class="chart-container">
+                        <canvas id="myAreaChartTop"></canvas>
                     </div>
                     <!-- Inputs to update chart -->
                     <div class="form-group mt-4">
@@ -130,23 +142,23 @@
                     </div>
                     <div class="form-group mt-4">
                         <label for="fixedCost">Costos Fijos (Cf):</label>
-                        <input type="number" id="fixedCost" class="form-control" value="2000" disabled>
+                        <input type="number" id="fixedCost" class="form-control" value="1000" disabled>
                         <small id="fixedCostHelp" class="form-text text-muted">
                             Ejemplos de costos fijos: alquiler, salarios, servicios públicos, seguros, licencias, mantenimiento de equipos, impuestos de propiedad, marketing y publicidad, servicios de limpieza, software y herramientas.
                         </small>
                     </div>
-                    <button id="updateChart" class="btn btn-primary">Actualizar Gráfica</button>
+                    <button id="updateChartTop" class="btn btn-primary">Actualizar Gráfica</button>
 
                     <!-- Additional Information -->
-                    <div id="additionalInfo" class="mt-4">
-                        <p><strong>Ganancias Calculadas (G):</strong> <span id="calculatedEarnings"></span></p>
+                    <div id="additionalInfoTop" class="mt-4">
+                        <p><strong>Ganancias Calculadas (G):</strong> <span id="calculatedEarningsTop"></span></p>
                     </div>
                 </div>
             </div>
         </div>
 
-        <!-- Pie Chart -->
-        <div class="col-xl-4 col-lg-5">
+        <!-- Fuentes de Ingreso Mensual Pie Chart -->
+        <div class="col-xl-4 col-lg-4">
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -166,7 +178,7 @@
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
-                    <div class="chart-pie pt-4 pb-2">
+                    <div class="chart-pie-container">
                         <canvas id="myPieChart"></canvas>
                     </div>
                     <div class="mt-4 text-center small">
@@ -198,8 +210,8 @@
 
 <!-- Chart.js -->
 <script>
-    var ctx = document.getElementById('myAreaChart').getContext('2d');
-    var myAreaChart = new Chart(ctx, {
+    var ctxTop = document.getElementById('myAreaChartTop').getContext('2d');
+    var myAreaChartTop = new Chart(ctxTop, {
         type: 'line',
         data: {
             labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
@@ -223,38 +235,75 @@
         }
     });
 
-    document.getElementById('updateChart').addEventListener('click', function() {
-        const Q = parseInt(document.getElementById('quantity').value);
-        const Po = parseInt(document.getElementById('originalPrice').value);
-        const Pn = parseInt(document.getElementById('newPrice').value);
-        const Cf = parseInt(document.getElementById('fixedCost').value); // Costos fijos predefinidos
+    var ctxPie = document.getElementById('myPieChart').getContext('2d');
+    var myPieChart = new Chart(ctxPie, {
+        type: 'pie',
+        data: {
+            labels: ['Ingreso Página Web', 'Local', 'Otros'],
+            datasets: [{
+                data: [55, 30, 15],
+                backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+                hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+                hoverBorderColor: 'rgba(234, 236, 244, 1)',
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                backgroundColor: 'rgb(255,255,255)',
+                bodyFontColor: '#858796',
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+            },
+            legend: {
+                display: false
+            },
+            cutoutPercentage: 80,
+        },
+    });
 
+    document.getElementById('updateChartTop').addEventListener('click', function() {
+        var Q = parseInt(document.getElementById('quantity').value);
+        var Po = parseInt(document.getElementById('originalPrice').value);
+        var Pn = parseInt(document.getElementById('newPrice').value);
+        var Cf = parseInt(document.getElementById('fixedCost').value); // Costos fijos predefinidos
+        
         if (Q && Po && Pn) {
-            const G = Q * (Pn - Po) - Cf; // Fórmula simplificada
-
-            // Actualiza el gráfico con el nuevo dato
-            myAreaChart.data.datasets[0].data.push(G);
-            myAreaChart.data.labels.push(getNextLabel(myAreaChart.data.labels));
-            myAreaChart.update();
+            var G = Q * (Pn - Po) - Cf; // Fórmula simplificada
+            
+            // Actualiza el gráfico superior con el nuevo dato
+            myAreaChartTop.data.datasets[0].data.push(G);
+            myAreaChartTop.data.labels.push(getNextLabel(myAreaChartTop.data.labels));
+            myAreaChartTop.update();
 
             // Actualiza la información adicional
-            document.getElementById('calculatedEarnings').textContent = "$" + G.toFixed(2);
+            document.getElementById('calculatedEarningsTop').textContent = "$" + G;
         }
     });
 
     function getNextLabel(labels) {
-        const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-        const lastLabel = labels[labels.length - 1];
-        let [lastMonth, lastYear] = lastLabel.split(' ');
-        lastYear = lastYear ? parseInt(lastYear) : new Date().getFullYear();
-
-        let nextMonthIndex = months.indexOf(lastMonth) + 1;
+        var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+        var lastLabel = labels[labels.length - 1];
+        var year = new Date().getFullYear();
+        var [lastMonth, lastYear] = lastLabel.split(' ');
+        
+        var nextMonthIndex = months.indexOf(lastMonth) + 1;
         if (nextMonthIndex >= months.length) {
             nextMonthIndex = 0;
-            lastYear++;
+            year++;
         }
 
-        return `${months[nextMonthIndex]} ${lastYear}`;
+        var nextMonth = months[nextMonthIndex];
+        if (lastYear) {
+            year = parseInt(lastYear);
+            return `${nextMonth} ${nextMonthIndex === 0 ? year + 1 : year}`;
+        } else {
+            return `${nextMonth} ${nextMonthIndex === 0 ? year + 1 : year}`;
+        }
     }
 </script>
 
