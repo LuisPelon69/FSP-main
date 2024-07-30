80b587f95b25fcffb9006ed90faec2d4bc8b161a
AlfredoFlores <floresibarraalfredo@gmail.com>
2024-07-30 12:09:30 -0600

    favicon

--- View/AutenticacionVista.php
+++ View/AutenticacionVista.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-
+    <link rel="icon" type="image/x-icon" href="../img/icono.png">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
         <link rel="stylesheet" href="../css/Style_login.css"> <!-- Ruta al archivo CSS -->
 
-    <title>Login</title>
+    <title>FSP - Login</title>
 </head>
 <body>
     <div class="container" id="container">
--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>FSP</title>
+    <link rel="icon" type="image/x-icon" href="img/icono.png">
     <link rel="stylesheet" type="text/css" href="../FSP-main-2/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../FSP-main-2/css/Style.css">
     <link rel="stylesheet" type="text/css" href="../FSP-main-2/css/index2.css">
